84112f1a3ae950046eaf697c385239fdda977fea
krzys1ek <combat100@gmail.com>
2017-04-20 21:10:43 +0200

    correct address

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const API_KEY = 'appid=ca48c0670ad1f12ebd212d6f2bc758ec';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?`;
+const API_KEY = 'ca48c0670ad1f12ebd212d6f2bc758ec';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FEATCH_WEATHER = 'FEATCH_WEATHER';
 
 export function fetchWeather(city) {
-  const url = `${ROOT_URL}q=${city},us&${API_KEY}`;
+  const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
   return {
     type: FEATCH_WEATHER,
